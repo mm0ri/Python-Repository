f5695b97d1a6d8f61bc60c9deafaa70c81984042
Vadim <Shkuratvadim@gmail.com>
2022-01-30 20:17:41 +0300

    fix
    fixed some logic problems

--- Build Tower/TowerBuild.py
+++ Build Tower/TowerBuild.py
@@ -2,10 +2,10 @@ n_floors = 3
 
 def tower_builder(n_floors):
     res_list = ['*']
-    i = 0
+    i = 1
     while i < n_floors:
-        res_list.append(res_list[i] + '**')
+        res_list.append(res_list[i-1] + '**')
         i = i + 1
-        
+    print(res_list)
 
 tower_builder(n_floors)
