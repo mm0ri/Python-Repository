d525a5a4e7535b569f72069aa14a68f40a5d2822
Vadim <Shkuratvadim@gmail.com>
2022-01-30 20:56:28 +0300

    TowerBuild was updated
    Final version on kata.

--- Build Tower/TowerBuild.py
+++ Build Tower/TowerBuild.py
@@ -1,11 +1,18 @@
 n_floors = 3
 
 def tower_builder(n_floors):
-    res_list = ['*']
+    buff_list = ['*']
     i = 1
+    s = 0
     while i < n_floors:
-        res_list.append(res_list[i-1] + '**')
+        buff_list.append(buff_list[i-1] + '**')
         i = i + 1
-    print(res_list)
+    for element in buff_list:
+        while len(buff_list[s]) < len(buff_list[-1]):
+            buff_list[s] = ' ' + buff_list[s] + ' '
+        s = s + 1
+
+    print(buff_list)
+    return buff_list
 
 tower_builder(n_floors)
