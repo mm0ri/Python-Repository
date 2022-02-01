6ea0e575288c82760279394d7af05d3a4c1ccd66
Vadim <Shkuratvadim@gmail.com>
2022-02-01 23:12:47 +0300

    Updated version of project
    Not completed project, but i'm in progress.

--- Sudoku Validator/SudokuValidator.py
+++ Sudoku Validator/SudokuValidator.py
@@ -1,13 +1,28 @@
-board =[[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-        [6, 7, 2, 1, 9, 5, 3, 4, 8],
-        [1, 9, 8, 3, 4, 2, 5, 6, 7],
-        [8, 5, 9, 7, 6, 1, 4, 2, 3],
-        [4, 2, 6, 8, 5, 3, 7, 9, 1],
-        [7, 1, 3, 9, 2, 4, 8, 5, 6],
-        [5, 6, 1, 5, 3, 7, 2, 8, 4],
-        [2, 8, 7, 4, 1, 9, 6, 3, 5],
-        [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+board =[[1, 3, 2, 5, 7, 9, 4, 6, 8], 
+        [4, 9, 8, 2, 6, 1, 3, 7, 5], 
+        [7, 5, 6, 3, 8, 4, 2, 1, 9], 
+        [6, 4, 3, 1, 5, 8, 7, 9, 2], 
+        [5, 2, 1, 7, 9, 3, 8, 4, 6], 
+        [9, 8, 7, 4, 2, 6, 5, 3, 1], 
+        [2, 1, 4, 9, 3, 5, 6, 8, 7], 
+        [3, 6, 5, 8, 1, 7, 9, 2, 4], 
+        [8, 7, 9, 6, 4, 2, 1, 3, 5]]
 
 def valid_solution(board):
+        flag = 0
+        for row in board:
+                #print("-----")
+                for elem in row:
+                        count_res = row.count(elem)
+                        if count_res > 1:
+                                flag = 1
+                        print("elem", elem)
+                        print("count", row.count(elem))
+        if flag == 1:
+                print("False")
+                return False
+        else:
+                print("True")
+                return True
 
 valid_solution(board)
