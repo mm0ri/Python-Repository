4f17c2774cd448c154d9af66911b3f9efd287bf8
Vadim <Shkuratvadim@gmail.com>
2022-02-03 14:32:03 +0300

    DetectPangram was updated!
    Some logic fix

--- Detect Pangram/DetectPangram.py
+++ Detect Pangram/DetectPangram.py
@@ -3,11 +3,10 @@ s = 'Aacdefghijklmnopqrstuvwxyz'
 def is_pangram(s):
     alph = "abcdefghijklmnopqrstuvwxyz"
     for char in alph:
+        print(char)
         if char not in s.lower():
             print("False")
             return False
-        else:
-            print("True")
-            return True
+    return True
 
 is_pangram(s)
