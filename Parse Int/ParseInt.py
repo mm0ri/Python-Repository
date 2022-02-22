df03abba4d3c1bd51a3e7c34f7e0fcb3fb3d269d
Vadim <Shkuratvadim@gmail.com>
2022-02-22 20:01:24 +0300

    Some little progress

--- Parse Int/ParseInt.py
+++ Parse Int/ParseInt.py
@@ -1,3 +1,18 @@
-string = ''
-def parse_int(string):
-    return
+string = 'two hundred fifty five'
+def parse_int():
+
+    def hundred():
+        pass
+    def thousand():
+        pass
+
+    NUMBER_CODE = { 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9', 
+                'ten':'10', 'eleven':'11', 'twelve':'12', 'thirteen':'13', 'fourteen':'14', 'fiveteen':'15', 'sixteen':'16', 'seventeen':'17', 'eighteen':'18', 'nineteen':'19',
+                'twenty':'20', 'thirty':'30', 'fourty':'40', 'fifty':'50', 'sixty':'60', 'seventy':'70', 'eighty':'80', 'ninety':'90', 'hundred':hundred(), 'thousand':thousand() }
+                
+    buff_list = string.split(' ')
+
+    print(buff_list)
+    #return
+
+parse_int()
