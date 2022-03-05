47a2d3f9961380e511ad609f2b68ce0595e2117e
Vadim <Shkuratvadim@gmail.com>
2022-03-06 01:36:52 +0300

    Some changes.
    Still not completed.

--- Parse Int/ParseInt.py
+++ Parse Int/ParseInt.py
@@ -1,5 +1,8 @@
 string = 'two hundred fifty five'
 def parse_int():
+    buff_list = string.split(' ')
+    res_list = []
+
     var = 2
     def hundred(var):
         var = str(var*100)
@@ -12,12 +15,13 @@ def parse_int():
                 'ten':'10', 'eleven':'11', 'twelve':'12', 'thirteen':'13', 'fourteen':'14', 'fiveteen':'15', 'sixteen':'16', 'seventeen':'17', 'eighteen':'18', 'nineteen':'19',
                 'twenty':'20', 'thirty':'30', 'fourty':'40', 'fifty':'50', 'sixty':'60', 'seventy':'70', 'eighty':'80', 'ninety':'90', 'hundred':hundred(var), 'thousand':thousand(var) }
 
-    buff_list = string.split(' ')
-    res_list = []
     for element in buff_list:
-        print(element)
-        res_list.append(element.replace(element, NUMBER_CODE[element]))
-    #print(hundred(var))
+        if element == 'hundred':
+            res_list.append(buff_list[element-1] * 100)
+        if element == 'thousand':
+            res_list.append(buff_list[element-1] * 1000)
+
+
     print(res_list)
     #return
 
