892180f8a2c1e3ce774fa6d3b257f051c4ffeaa5
Vadim <Shkuratvadim@gmail.com>
2022-03-10 17:52:00 +0300

    upd

--- Learn Objects/CarObjects.py
+++ Learn Objects/CarObjects.py
@@ -1,2 +1,10 @@
 class Car():
-    None
+    
+    def __init__(self, color, manufacturer, year, horse_power, weight):
+        self.__color = color
+        self.__manufacturer = manufacturer
+        self.__horse_power = horse_power
+        self.__weight = weight
+
+    def calc_power(self, weight, horse_power):
+        None
