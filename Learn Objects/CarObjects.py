80f53206357346169d783a32d09c77a99dca5070
Vadim <Shkuratvadim@gmail.com>
2022-03-11 02:28:12 +0300

    upd

--- Learn Objects/CarObjects.py
+++ Learn Objects/CarObjects.py
@@ -6,5 +6,11 @@ class Car():
         self.__horse_power = horse_power
         self.__weight = weight
 
+    def set_speed(self):
+        None
+
+    def set_weight(self):
+        None
+
     def calc_power(self, weight, horse_power):
         None
