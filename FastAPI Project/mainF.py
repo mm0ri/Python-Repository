d450d2c79735341bed3552198eaa7407959dd744
Vadim <Shkuratvadim@gmail.com>
2022-03-15 01:17:02 +0300

    Updates

--- FastAPI Project/mainF.py
+++ FastAPI Project/mainF.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
 
-@app.get("/items/{item_id}&{item_id2}")
-async def read_item(item_id: int, item_id2: int):
-    return {"item_id": item_id, "item_id2": item_id2}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{name}&{message}", response_class=HTMLResponse)
+async def read_item(request: Request, name: str, message: str):
+    return templates.TemplateResponse("item.html", {"request": request, "name": name, "message": message})
